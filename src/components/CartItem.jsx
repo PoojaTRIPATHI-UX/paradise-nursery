7d8e7c9e9e29b7b524abafd2e4ba753e7e1612b7
PoojaTRIPATHI-UX <tripathipooja052@gmail.com>
2026-06-18 14:00:31 +0530

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,1 +1,20 @@
+import { Link } from 'react-router-dom'
 
+export default function CartItem({ item, updateQty, removeItem }) {
+  return (
+    <div className="cart-item">
+      <img src={item.image} alt={item.name} className="cart-thumbnail" />
+      <div className="cart-info">
+        <h4>{item.name}</h4>
+        <p>Unit Price: ${item.price}</p>
+        <p>Total: ${(item.price * item.qty).toFixed(2)}</p>
+      </div>
+      <div className="qty-controls">
+        <button onClick={() => updateQty(item.id, -1)}>-</button>
+        <span>{item.qty}</span>
+        <button onClick={() => updateQty(item.id, 1)}>+</button>
+      </div>
+      <button onClick={() => removeItem(item.id)} className="delete-btn">Delete</button>
+    </div>
+  )
+}
