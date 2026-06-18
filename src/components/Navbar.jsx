c1650d2d0f6eef316578973d411998b3ae456292
PoojaTRIPATHI-UX <tripathipooja052@gmail.com>
2026-06-18 14:07:28 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,14 @@
+import { Link } from 'react-router-dom'
 
+export default function Navbar({ cartCount }) {
+  return (
+    <nav className="navbar">
+      <h1>🌿 Paradise Nursery</h1>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/plants">Plants</Link>
+        <Link to="/cart">Cart ({cartCount})</Link>
+      </div>
+    </nav>
+  )
+}
